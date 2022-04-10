5460b7367270d0331cc4c52201d2e067aa8b37fc
Dean-SF <dsanabriaf@estudiantec.cr>
2022-04-09 18:02:16 -0600

    Plantilla de trabajo

---
+++ Classes/Punto.h
@@ -0,0 +1,14 @@
+#ifndef PUNTO_H
+#define PUNTO_H
+
+class Punto
+{
+private:
+    /* data */
+public:
+    Punto(/* args */);
+    ~Punto();
+};
+
+
+#endif
---
+++ Makefile
@@ -0,0 +1,2 @@
+Main:
+	g++ -static -std=c++17 main.cpp -o main && .\main.exe
