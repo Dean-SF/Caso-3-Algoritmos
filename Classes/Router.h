b3ce7dbbdc75e089f2d0369d8b132c1b6ac20571
Dean-SF <dsanabriaf@estudiantec.cr>
2022-04-15 19:38:20 -0600

    documentacion

--- Classes/Router.h
+++ Classes/Router.h
@@ -28,20 +28,32 @@ private:
     Observer* animator;
     TypeOfRoute typeOfRoute;
     xml_document* docPointer;
-    vector<Point*> distances;
+    vector<Point*> distances; // resulting vector as a attribute
     int processId;
-
-    Resolution canvasSize;
+    
+    Resolution canvasSize; // width and height as a Resolution object
     double angle;
     int frames;
-    int quadrant;
+    int quadrant; // Quadrant as atribute
 
-    // Debug only, remove for final release1
+    /*
+    Resumen:
+    verifySpecialCase() -> elimitado, en cambio primera etapa es en calculateQuadrantNormalCase(), donde
+    se va a transformar el caso especial en un angulo de 0 o 90, y se le asigna un quadrante para identificar
+    si se mueve hacia arriba, abajo, izquierda o derecha. En caso de ser un caso especial ( que ahora es reducido
+    a un angulo de 0 o 90), se ejecuta calculateRouteForSpecialCase(), si no calculateRouteForNormalCase(). ambas
+    funciones se completaron y estan explicadas en la debida funcion.
+    Ademas se redujeron los parametros en funciones y se cambiaron a atributos de la clase para que puedan ser
+    accedidos de cualquier lado
+    */
+
+    // Debug only, remove for final release just to print the resulting vector
     void printDistances() {
         for(Point *actual : distances) {
              cout << "(" << actual->getHorizontalAxis() << "," << actual->getVerticalAxis() << ")" << endl;
         }
     }
+
 
     void calculateQuadrantNormalCase() {
         double rightLimit, leftLimit, upperLimit, lowerLimit;
@@ -83,15 +95,26 @@ private:
     }
 
     void calculateRouteForSpecialCase() {
+
+        // Debugging
         cout << "Process for special case" << endl;
         
         cout << "Cuadrant = " << quadrant << endl;
         cout << "Angle = " << angle << endl;
-        cout << "prueba" << endl;
 
-        xml_node mainSvgNode = docPointer->child("svg").last_child();
-        xml_node_iterator nodeIterator;
+        // Empieza la funcion
 
+        /*
+        CAMBIO DE PLANTILLA: Se puso un nodo SVG al final del nodo SVG principal, entonces 
+        puede ser accedido al instante y todo lo que esta dentro de ese nodo va ser un nodo SVG con mask 
+        y coordenadas para evitar comparar strings, lo que es muy lento
+        */
+
+        // mainSvgNode contiene el nodo con todos los nodos que traen los datos que se necesitan
+        xml_node mainSvgNode = docPointer->child("svg").last_child(); 
+
+
+        xml_node_iterator nodeIterator; // recorrido por los nodos para obtener coordenadas y calcular direcciones
         for(nodeIterator = mainSvgNode.begin(); nodeIterator != mainSvgNode.end(); nodeIterator++) {
             int coordinate;
 
@@ -116,10 +139,11 @@ private:
                     break;
             }
         }
-        
-        
+        printDistances(); // for debugging
         notify(docPointer);
     }
+
+
 
     void calculateRouteForNormalCase() {        
         
@@ -128,22 +152,34 @@ private:
         cout << "Cuadrant = " << quadrant << endl;
         cout << "Angle = " << angle << endl;
         
-        int xAxis,yAxis;
-        int invertSign = 1;
+        int xAxis,yAxis; 
+        
+        // leg significa cateto, por lo tanto las primeras variables son "primer cateto y segundo cateto"
+        // slope va ser la pendiente(m) y linearConstant la b en la funcion (lineal) y = mx + b 
         double firstLeg,secondLeg,slope,linearConstant;; // leg -> cateto
 
-        
-
         xml_node node;
+
+        // Esta parte y los parametro del for son iguales a la anterior
         xml_node mainSvgNode = docPointer->child("svg").last_child();
-
         xml_node_iterator nodeIteratorParent; ;
-        
         for(nodeIteratorParent = mainSvgNode.begin(); nodeIteratorParent != mainSvgNode.end(); nodeIteratorParent++) {
-            node = *nodeIteratorParent;
-            xAxis = stoi(node.child("mask").child("rect").attribute("x").value());;
-            yAxis = stoi(node.child("mask").child("rect").attribute("y").value());;
 
+            // Extraccion de coordenadas fue cambiado, originalmente necesitaba de dos variables extras
+            node = *nodeIteratorParent;
+            xAxis = stoi(node.child("mask").child("rect").attribute("x").value());
+            yAxis = stoi(node.child("mask").child("rect").attribute("y").value());
+
+            /*
+            Para el primer y cuarto cuadrante siempre se usa la misma distancia en el eje X para el primer
+            cateto puesto que estan reflejados en dicho eje
+            */
+
+            /*
+            Para el segundo y tercer cuadrante (parte del "else") siempre se usa la misma distancia del eje x
+            la cual es el valor del xAxis
+            */
+        
             if(quadrant == 1 || quadrant == 4) {
                 firstLeg = canvasSize.getWidth() - xAxis;
                 secondLeg = tan(angle)*firstLeg;
@@ -152,9 +188,30 @@ private:
                 secondLeg = tan(angle)*firstLeg;
             }
 
+            /*
+            Para los cuadrantes 1 y 2 si el triangulo cuadrado se sale de los limites se verifica
+            si la distancia del segundo cateto (opuesto) es mayor a la del valor de yAxis del punto actual
+
+            Para los cuadrantes 3 y 4 si el triangulo cuadrado se sale de los limites se verifica que 
+            la coordenada 'y' del punto de llegada no sobrepase el limite del canvas
+
+            En cualquier caso si se supera el limite, se calcula la pendiente y la 'b' de la funcion
+            lineal para usar la funcion inversa y obtener una coordenada en el eje X que choque con
+            el limite del canvas, una vez se hace eso:
+
+            Para cuadrante 1 y 2 el segundo cateto (opuesto) va ser del tamaño del yAxis, y
+            el primer cateto (adyacente) va ser la distancia entre la coordenada del eje x encontrada
+            con la funcion inversa y xAxis que era el punto inicial
+
+            Para cuadrante 3 y 4 al pasarse el limite significa que la distancia del segundo cateto
+            (opuesto) es la distancia entre el final del canvas y el yAxis del punto actual, mientras
+            que el primer cateto (adyacente) es la distancia entre la coordenada del eje x encontrada
+            con la funcion inversa, pero esta vez se usa el valor maximo de altura del canvas, y 
+            el xAxis del punto inicial
+            */
             if((secondLeg > yAxis && (quadrant == 1 || quadrant == 2)) || 
-               (secondLeg > canvasSize.getHeight() && (quadrant == 3 || quadrant == 4))) {
-                slope = (secondLeg)/(firstLeg);
+               (secondLeg + yAxis > canvasSize.getHeight() && (quadrant == 3 || quadrant == 4))) {
+                slope = (secondLeg)/(firstLeg);     
                 linearConstant = yAxis-(slope*xAxis);
                 if(quadrant == 1 || quadrant == 2) {
                     secondLeg = yAxis;
@@ -164,7 +221,9 @@ private:
                     firstLeg = (((canvasSize.getHeight()-linearConstant)/slope) - xAxis);
                 }
             }
+            
 
+            // Esto es para acomodar los signo de los valores, segun el cuadrante
             if(quadrant == 2 || quadrant == 3)
                 firstLeg *= -1;
             if(quadrant == 1 || quadrant == 2)
@@ -183,7 +242,7 @@ public:
         angle = pAngle;
         frames = pFrames;
         typeOfRoute = pTypeOfRoute;
-        canvasSize.setViewBoxResolution(pDocPointer->child("svg").attribute("viewBox").value(),true);
+        canvasSize.setViewBoxResolution(pDocPointer->child("svg").attribute("viewBox").value(),true); // set canvas size;
     }
     ~Router() {} 
 
@@ -208,7 +267,7 @@ public:
     }
 
     void setWidth(int pWidth) {
-        canvasSize.setWidth(pWidth);
+        canvasSize.setWidth(pWidth);    
     }
 
     void setHeight(int pHeight) {
