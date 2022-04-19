54c906ecf5d3f34e6cc893cfe995b9332ce31c71
Dean-SF <dsanabriaf@estudiantec.cr>
2022-04-19 15:20:34 -0600

    Merge de archivos de la branch de Andrea
    - Se obtuvo el ObserverPattern.h

---
+++ Classes/Generator.h
@@ -0,0 +1,38 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include "ObserverPattern.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using pugi::xml_document;
+
+class Generator : public Observer {
+private:
+    int processId;
+
+public:
+    Generator() {
+        processId = 2;
+    }
+    ~Generator() {}
+
+    void work(xml_document* pDocPointer, void* pCoordinates) {
+        cout << "Working..." << endl;
+        cout << "Animation complete" << endl;
+    }
+
+    void update(xml_document* pDocPointer, void* pCoordinates) {
+        cout << "Generator started working" << endl;
+        work(pDocPointer, pCoordinates);
+    }
+
+    int getProcessId() {
+        return processId;
+    }
+
+};
+
+#endif // GENERATOR_H
---
+++ Classes/ObserverPattern.h
@@ -0,0 +1,23 @@
+#ifndef OBSERVERPATTERN_H
+#define OBSERVERPATTERN_H
+
+#include "../libraries/pugixml/pugixml.hpp"
+
+using pugi::xml_document;
+
+class Observer {
+public:
+  virtual ~Observer(){};
+  virtual void update(xml_document* pElement, void* pCoordinates) = 0;
+  virtual int getProcessId() = 0;
+};
+
+class Subject {
+public:
+  virtual ~Subject(){};
+  virtual void attach(Observer *pObserver) = 0;
+  virtual void detach(Observer *pObserver) = 0;
+  virtual void notify(xml_document* pElement, void* pCoordinates) = 0;
+};
+
+#endif // OBSERVERPATTERN_H
